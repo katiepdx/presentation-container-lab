87dbda7fc8f6eb41a48ddce73bc9541522c495be
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-21 14:22:03 -0700

    update test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import Loading from './presentational/loading';
+import Loading from './presentational/Loading';
 
 describe('App component', () => {
   afterEach(() => cleanup());
