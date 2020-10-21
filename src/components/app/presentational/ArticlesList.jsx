3c659d93b846cf17839c210da19639a925040dc1
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-21 15:20:09 -0700

    add api fetch functionality

--- src/components/app/presentational/ArticlesList.jsx
+++ src/components/app/presentational/ArticlesList.jsx
@@ -3,23 +3,36 @@
 
 import React, { Component } from 'react'
 import Loading from '../presentational/Loading'
+import { getNewsArticles } from '../services/news-api'
 
 export default class ArticlesList extends Component {
-  // set state - loading is true 
+  // set state 
   state = {
-    loading: true
+    loading: true,
+    userSearch: 'bitcoin',
+    foundArticles: []
+  }
+
+  // when page loads, fetch from API, update state, and change loading to false
+  async componentDidMount() {
+    // fetch from api using user's search
+    const foundArticles = await getNewsArticles();
+  
+    // set state 
+    this.setState({ foundArticles: foundArticles, loading: false})
   }
 
   render() {
     // deconstruct state 
-    const { loading } = this.state;
+    const { loading, foundArticles } = this.state;
 
+    console.log(foundArticles, 'ARTICLES LIST IN STATE IN RENDER')
     // conditional rendering 
     if(loading) return <Loading />
 
-    else return (
+    return (
       <div>
-        Not loading 
+        not loading
       </div>
     )
   }
--- src/components/app/services/news-api.js
+++ src/components/app/services/news-api.js
@@ -1,1 +1,9 @@
-// getNews API fetch logic
+// getNewsArticles API fetch logic
+// TO DO: UPDATE search 
+
+export const getNewsArticles = async() => {
+  const res = await fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`)
+  const data = await res.json();
+  const articlesList = await data.articles;
+  return articlesList;
+}
